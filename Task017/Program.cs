59bc421d3fcd53a1e59bc428d036fa7a4135e0ff
«Alexandra» <sashatoropova@gmail.com>
2022-05-15 14:52:57 +0700

    #17

---
+++ Task017/Program.cs
@@ -0,0 +1,21 @@
+﻿// определение плоскости координат
+Console.Write("Введите значение по оси х: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Dведите значение по оси y: ");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
