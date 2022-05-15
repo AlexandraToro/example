6a5469148586a40409e8db503fe7572aa304fdcb
«Alexandra» <sashatoropova@gmail.com>
2022-05-15 14:52:22 +0700

    #14

---
+++ Task014/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.WriteLine("Определние кратности 7  и 23");
+Console.WriteLine("Введите чисоj");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine (number%7 == 0 && number%23 == 0 ? $" {number}- > кратно" : $" {number} - > не кратно");
