2cfdb5b0876f8b47f6fa496f5db5f2bb0a6bb648
«Alexandra» <sashatoropova@gmail.com>
2022-05-15 14:52:01 +0700

    #10

---
+++ Task010/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int firstDigit = num / 100;
+int secondDigit = num % 10;
+Console.WriteLine($"{firstDigit}{secondDigit}");
+
+Console.WriteLine(num/100*10+num%10);
