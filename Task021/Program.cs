90ad39639c7f3515ef030b37cb1efab7de15516f
«Alexandra» <sashatoropova@gmail.com>
2022-05-15 15:20:42 +0700

    задача21.1

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -8,10 +8,10 @@ int xB = int.Parse(Console.ReadLine());
 Console.WriteLine("Ввод координаты y точки B");
 int yB = int.Parse(Console.ReadLine());
 
-float getDist(int x1,int y1, int x2, int y2)
+float getDist(int x1, int y1, int x2, int y2)
 {
-float result= (float)(Math.Sqrt((Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2))));
-return result;
+    float result = (float)(Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))));
+    return result;
 }
 
-Console.WriteLine("{0: 0.00}" ,getDist(xA, yA, xB,yB));
+Console.WriteLine("{0: 0.00}", getDist(xA, yA, xB, yB));
