71f70ef0bb5510ba3e447d840cdd02e58ffa837e
«Alexandra» <sashatoropova@gmail.com>
2022-05-18 14:37:40 +0700

    add task022

---
+++ Task022/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+void TablQuart(int number)
+{
+Console.WriteLine("|{0,10}|{1,10}|", "Число", "Квадрат числа");
+for (int count = 1; count <= num; count++)
+{
+    Console.WriteLine("|{0,10}|{1,13}|", count, count * count);
+}
+}
+TablQuart(num);
