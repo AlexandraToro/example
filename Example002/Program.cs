722801b76e5a669887477e389529ad691c6252f1
«Alexandra» <sashatoropova@gmail.com>
2022-05-18 14:47:15 +0700

    new solution

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,3 +1,10 @@
 ﻿Console.WriteLine ("Введите число ");
 int a = int.Parse(Console.ReadLine());
-Console.WriteLine(a*a);
+
+int numSquare(int number)
+{
+int res=a*a;
+return res;
+}
+ int result = numSquare(a);
+ Console.WriteLine($"Квадрат числа равен {result}");
