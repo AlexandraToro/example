dd43dada8c5f0eb8c06f671db1baff7bb35a97a8
«Alexandra» <sashatoropova@gmail.com>
2022-05-21 14:29:55 +0700

    add task024

---
+++ Task024/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите любое натуральное число");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"{number} -> {sum}");
