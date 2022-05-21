db44f2e92d6d6f42570b25ed341c065f42923d10
«Alexandra» <sashatoropova@gmail.com>
2022-05-21 14:58:56 +0700

    add task026

---
+++ Task026/Program.cs
@@ -0,0 +1,11 @@
+﻿// 
+Console.WriteLine("Введите любое натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+int i = 0;
+Console.Write($"Количество цифр в числе {number} равно ");
+while (number > 0)
+{
+    number = number / 10;
+    i++;
+}
+Console.WriteLine (i);
