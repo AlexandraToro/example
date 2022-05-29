4ac2d455541f87c2ec2e73b3bda19e79914c7c61
«Alexandra» <sashatoropova@gmail.com>
2022-05-29 16:30:04 +0700

    add task 028+030

---
+++ Task028/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу которая принимает на вход сило и выдает произведение числа от 1 до введенного числа
+Console.WriteLine("Введите любое натуральное число");
+int number = int.Parse(Console.ReadLine());
+double mul = 1;
+for (int i = 1; i <= number; i++)
+{
+    mul = mul * i;
+}
+Console.WriteLine($"Факториал числа {number} равен {mul}");
